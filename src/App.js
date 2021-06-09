9d105578ba58118c1cf7e0a65829d61ceaad0f32
carl0sfelipe <37373901+carl0sfelipe@users.noreply.github.com>
2021-06-09 12:43:04 -0300

    Revert "Banner"

--- src/App.js
+++ src/App.js
@@ -1,35 +1,24 @@
 import React from 'react';
-import astronalt from './images/astronaut-resized.png';
+import logo from './logo.svg';
 import './App.css';
-import banner from './images/banner.webp'
-import styled from 'styled-components'
-
-
-
-const Banner = styled.img`
-display: felx;
-justify-content: center;
-width: 100vw;
-max-height: 70vh;
-`
-
-const Astronalt = styled.img`
-position: absolute;
-top:-33px;
-right:30vw ;
-width: 20%;
-transition: transform 1s;
-&:hover{
-  transform: translate(20px, 20px);
-}
-`
 
 function App() {
   return (
-    <div className="banner">
-      <Banner src={banner} className="banner-img" ></Banner>
-        <Astronalt src={astronalt} className="astronalt-img" alt="astronalt" ></Astronalt>
-        
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
     </div>
   );
 }
