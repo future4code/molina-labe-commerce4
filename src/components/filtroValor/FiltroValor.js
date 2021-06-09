3e8597172e0e8133fcd1610f459eb66e48e91a98
TIAGO PRESTES <83032703+Tiago-prestes@users.noreply.github.com>
2021-06-09 18:40:12 -0300

    Esqueleto React

---
+++ src/components/filtroValor/FiltroValor.js
@@ -0,0 +1,42 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const ListaFiltro = styled.label`
+    margin: 10px;
+`
+const MenuFiltro = styled.div`
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    flex-basis: 200px;
+    border-right-style: solid;
+    border-right-width: thick;
+`
+
+
+
+class FiltroValor extends React.Component {
+
+    render(){
+        return (
+            <MenuFiltro>
+                    <h3>Filtros</h3>
+                    <ListaFiltro>
+                    Valor mínimo:
+                    <input type="number" value></input>
+                    </ListaFiltro>
+                    <ListaFiltro>
+                    Valor máximo:
+                    <input type="number" value></input>
+                    </ListaFiltro>
+                    <ListaFiltro>
+                    Busca por nome:
+                    <input type="text" value="Produto"></input>
+                    </ListaFiltro>
+            </MenuFiltro>  
+        )    
+    }           
+}
+
+export default FiltroValor
+
