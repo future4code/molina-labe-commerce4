941b360e319b4b0ecc7bbc9b6b4313ea177d0938
carl0sfelipe <carlos.felipe@hotmail.com.br>
2021-06-09 12:42:18 -0300

    ajustes2

--- src/App.js
+++ src/App.js
@@ -1,35 +1,15 @@
 import React from 'react';
-import astronalt from './images/astronaut-resized.png';
 import './App.css';
-import banner from './images/banner.webp'
 import styled from 'styled-components'
+import Banner from './components/banner';
 
 
 
-const Banner = styled.img`
-display: felx;
-justify-content: center;
-width: 100vw;
-max-height: 70vh;
-`
-
-const Astronalt = styled.img`
-position: absolute;
-top:-33px;
-right:30vw ;
-width: 20%;
-transition: transform 1s;
-&:hover{
-  transform: translate(20px, 20px);
-}
-`
 
 function App() {
   return (
-    <div className="banner">
-      <Banner src={banner} className="banner-img" ></Banner>
-        <Astronalt src={astronalt} className="astronalt-img" alt="astronalt" ></Astronalt>
-        
+    <div >
+      <Banner></Banner>
     </div>
   );
 }
